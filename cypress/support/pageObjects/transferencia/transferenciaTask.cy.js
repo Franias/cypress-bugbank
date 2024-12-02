ac6f4241e01cfb7bd1cb626ee7ea63e8e276f088
Francielli Pinheiro Dias <francielli_dias@terceiros.sicredi.com.br>
2024-12-02 16:11:48 -0300

    resolve numeroConta e digitoConta

--- cypress/support/pageObjects/transferencia/transferenciaTask.cy.js
+++ cypress/support/pageObjects/transferencia/transferenciaTask.cy.js
@@ -2,23 +2,42 @@ import { homePage } from "../homePage.cy";
 import { transferenciaPage } from "./transferenciaPage.cy";
 
 export const realizaTransferencia = () => {
+    
+    cy.get('#textAccountNumber').contains('Conta digital: ').should('not.be.disabled');
     let numeroConta;
     let digitoConta;
-    // cy.get(homePage.numero_conta).invoke('text').then((text) => {
-    //     numeroConta = text.split('\-')[0];
-    //     digitoConta = text.split('\-')[1];
-    // });
-    
+    cy.wait(1000);    
     cy.get(homePage.numero_conta).then(($span) => {
         const txt = $span.text();
         cy.log(txt);
-
-    });
-
+        numeroConta = txt.split('\-')[0];
+        digitoConta = txt.split('\-')[1];
+        cy.log(numeroConta);
+        cy.log(digitoConta);
+        return cy.wrap({ numeroConta, digitoConta });
+      }).then((result) => {
+        cy.log(result.numeroConta);
+        cy.log(result.digitoConta);
+        // rest of your code here
+      });cy.get(homePage.numero_conta).then(($span) => {
+        const txt = $span.text();
+        cy.log(txt);
+        numeroConta = txt.split('\-')[0];
+        digitoConta = txt.split('\-')[1];
+        cy.log(numeroConta);
+        cy.log(digitoConta);
+      });
+      cy.wrap({ numeroConta, digitoConta }).then((result) => {
+        cy.log(result.numeroConta);
+        cy.log(result.digitoConta);
+        // rest of your code here
+        cy.get(homePage.botao_transferencia).click();
+        cy.get(transferenciaPage.numero_conta_input).type(numeroConta);
+        cy.get(transferenciaPage.numero_digito_input).type(digitoConta);
+    
+      });
     // cy.get(homePage.botao_transferencia).click();
-    // cy.get(transferenciaPage.numero_conta_input).type('559');
-    // cy.get(transferenciaPage.numero_digito_input).type('4');
-    // cy.get(transferenciaPage.valor_transferencia).type('100');
-    // cy.get(transferenciaPage.descricao).type('Descricao teste');//.type(numeroConta);
-    // cy.get('button').contains('Transferir agora').click({force: true});
+    cy.get(transferenciaPage.valor_transferencia).type('100');
+    cy.get(transferenciaPage.descricao).type('Descricao teste');//.type(numeroConta);
+    cy.get('button').contains('Transferir agora').click({force: true});
 }
